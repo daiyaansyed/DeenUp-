6fd2627e743bd6b52dc54df8769cbd6c7d23fb03
Daiyaan Syed <daiyaansyed@gmail.com>
2026-03-11 00:30:50 +0530

    Initial Commit

---
+++ HeadsUp/HeadsUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadsUpApp.swift
+//  HeadsUp
+//
+//  Created by Daiyaan Syed on 11/03/26.
+//
+
+import SwiftUI
+
+@main
+struct HeadsUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
